074b4d697b8c81c7645641c50a23f5483b3c00fb
kokaterimon <bpaucarm@outlook.com>
2020-05-30 08:46:01 -0500

    En el tutorial 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour{
+
+    public float jumpForce = 5f;
+
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Aquí, el usuario acaba de bajar la tecla espacio
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        //f = m * a ========> a = F/m
+        if (IsTouchingTheGround())
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public LayerMask groundLayer;
+
+    bool IsTouchingTheGround()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, groundLayer))
+        {
+
+        }
     }
 }
